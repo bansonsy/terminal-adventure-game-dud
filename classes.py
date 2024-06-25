cf0e27748e4f6b4a8089f538ec610bf93b5df29e
bansonsy <bansonsy@gmail.com>
2024-06-24 20:15:52 -0700

    Initial Commit

--- classes.py
+++ classes.py
@@ -17,3 +17,19 @@ class Room:
         print(self.description)
         print(self.smell)
         print(self.sound)
+
+
+class Game:
+    def __init__(self, player: Player):
+        self.player = player
+        self.Room = None
+        self.num_monsters: int = 0
+        self.rooms: dict = {}
+        self.x: int = 0
+        self.y: int = 0
+
+    def set_rooms(self, rooms: dict):
+        self.rooms = rooms
+
+    def set_current_room(self, room: Room):
+        self.room = room
